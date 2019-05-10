4b28c14ad086ba4c0d1f8020a574ec1d8b2ebcac
Stefanos Ugbit <stefanosu422@gmail.com>
2019-05-10 15:54:39 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,54 +1,60 @@
-# Write your code here.
-
-def dictionary
-  convert = {
-   "hello" => "hi",
-   "to" => "2",
-   "two" => "2",
-   "too" => "2",
-   "for" => "4",
-   "four" => "4",
-   "be" => "b",
-   "you" => "u",
-   "at" => "@",
-   "and" => "&"
+def dictionary 
+  {
+"hello" => 'hi',
+"to" => '2',
+"two" => '2',
+"too" => '2', 
+"for" => '4',
+"four" => '4',
+'be' => 'b',
+'you' => 'u',
+"at" => "@",
+"and" => "&"
 }
+
 end
 
-def word_substituter(tweet)
-  # replaces long words with their expected short form
-
-    tweet.split(" ").map do |word|
-      if dictionary.keys.include?(word.downcase)
-        word = dictionary[word.downcase]
-      else
-        word
-    end
-  end.join(" ")
+def word_substituter(str_tweets)
+  result = []
+  arr_dictionary = dictionary.keys
+  arr_tweets = str_tweets.split(' ')
+  arr_tweets.each do |word|
+    arr_dictionary.include?(word) ? result << dictionary[word] : result << word
+  end
+  result.join(' ')
 end
 
-def bulk_tweet_shortener(tweet)
-  tweet.map do |phrase|
-    puts word_substituter(phrase)
+def bulk_tweet_shortener(arr_of_tweets)
+  arr_of_tweets.map do |tweet|
+    word_substituter(tweet)
   end
 end
 
-def selective_tweet_shortener(tweet)
-  tweet.split(" ").map do |phrase|
-    if phrase.length > 140
-      word_substituter(phrase)
-    elsif phrase.length <= 140
-      phrase
-    end
-  end
+def selective_tweet_shortener(str_tweets)
+  str_tweets > 140 ? word_substituter(str_tweets) : str_tweets
 end
 
-def shortened_tweet_truncator(tweet)
-  tweet.split(" ").map do |phrase|
-    if phrase.length > 140
-      word_substituter(phrase)[0..140] + "..."
-    else
-      phrase
-    end
-  end.join(" ")
+def shortened_tweet_truncator(str_tweets)
+  str_tweets = str_tweets[0..139] + '...' if str_tweets > 140
+  str_tweets
 end
+
+tweet1 = 'hello how are you'
+tweet2 = 'you are cool at everything'
+tweet3 = 'for you to be at home you must be'
+longtweet = 'afsdfoi aoiuas fasdiouasdf asdfioausofahsdfj asdfoiasdfa asdfioausdoifasd asdfaosidufasdf asdofiuasoidfuaosdfs asdlfjaosidfsoidfuaoisd'
+
+puts longtweet.length
+
+# puts bulk_tweet_shortener([tweet1, tweet2, tweet3])
+
+# word_substituter('hello this is a test to you')
+
+def testfunc(str_tweets)
+  arr_tweets = str_tweets.split
+  
+  puts arr_tweets & dictionary.keys
+  
+end
+
+# testfunc('hello how are you doing')
